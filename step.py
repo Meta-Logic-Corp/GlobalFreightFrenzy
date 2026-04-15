0ac0b57b23c0facfeeda0563f75c5f040c0fc823
Kingxkoji <keirstonartis1@gmail.com>
2026-04-15 14:28:13 -0400

    Update step.py

--- step.py
+++ step.py
@@ -1,61 +1,68 @@
 from simulator import VehicleType
-from collections import deque
 import math
 
-def distance(coord1, coord2):
-    return math.sqrt((coord1[0]-coord2[0])**2 + (coord1[1]-coord2[1])**2)
+# Helper to calculate distance
+def get_dist(c1, c2):
+    return math.sqrt((c1[0]-c2[0])**2 + (c1[1]-c2[1])**2)
 
-def plan_route(box, hubs):
-    origin = box["location"]
-    dest = box["destination"]
-    
-    queue = deque([[origin]])
-    visited = {origin}
-    
-    while queue:
-        path = queue.popleft()
-        loc = path[-1]
-        
-        if distance(loc, dest) < 0.0005:
-            return path + [dest]
-        
-        for hub in hubs:
-            if hub not in visited:
-                visited.add(hub)
-                queue.append(path + [hub])
-    
-    return [origin, dest]
+# We use a global-style dictionary to track which boxes are already assigned
+# to prevent creating multiple vehicles for the same box.
+assigned_boxes = set()
 
 def step(sim_state):
-    if sim_state.tick == 0:
-        boxes = sim_state.get_boxes()
-        
-        # Discover hubs from box locations
-        hubs = set()
-        for box in boxes.values():
-            hubs.add(box["location"])
-            hubs.add(box["destination"])
-        hubs = list(hubs)
-        
-        for box in boxes.values():
-            if not box["delivered"]:
-                route = plan_route(box, hubs)
-                
-                # Only use SemiTruck (can unload anywhere)
-                try:
-                    vid = sim_state.create_vehicle(VehicleType.SemiTruck, route[0])
-                    sim_state.load_vehicle(vid, [box["id"]])
-                    sim_state.move_vehicle(vid, route[1])
-                except ValueError:
-                    pass  # Skip if can't spawn
+    global assigned_boxes
     
-    # Unload at destination
-    vehicles = sim_state.get_vehicles()
     boxes = sim_state.get_boxes()
-    
+    vehicles = sim_state.get_vehicles()
+
+    # 1. UNLOAD LOGIC (Check all vehicles to see if they reached their destination)
     for vid, v in vehicles.items():
-        if v["destination"] is None and v["cargo"]:
-            to_unload = [bid for bid in v["cargo"] 
-                        if distance(boxes[bid]["destination"], v["location"]) < 0.0005]
-            if to_unload:
-                sim_state.unload_vehicle(vid, to_unload)  # SemiTruck works at hubs
+        if v["cargo"] and v["destination"] is not None:
+            # Check if we are at the destination
+            # (Using a small threshold for GPS coordinates)
+            for bid in v["cargo"]:
+                box_dest = boxes[bid]["destination"]
+                if get_dist(v["location"], box_dest) < 0.01:
+                    # Unload the box
+                    sim_state.unload_vehicle(vid, [bid])
+                    # Once unloaded, the box is marked delivered by the API
+
+    # 2. DISPATCH LOGIC (Find new boxes to deliver)
+    for bid, box in boxes.items():
+        if not box["delivered"] and bid not in assigned_boxes:
+            # Check if the box is already on a vehicle
+            if box["vehicle_id"] is not None:
+                assigned_boxes.add(bid)
+                continue
+
+            dist = get_dist(box["location"], box["destination"])
+            
+            # Determine the best vehicle type
+            # If far away, Airplane is safest to avoid ocean penalties
+            if dist > 1000:
+                preferred_types = [VehicleType.Airplane, VehicleType.SemiTruck]
+            else:
+                preferred_types = [VehicleType.SemiTruck, VehicleType.Airplane]
+
+            # Attempt to spawn a vehicle
+            spawned = False
+            for vtype in preferred_types:
+                try:
+                    # Try to spawn at the box's current location
+                    vid = sim_state.create_vehicle(vtype, box["location"])
+                    
+                    # If successful, load and move
+                    sim_state.load_vehicle(vid, [bid])
+                    sim_state.move_vehicle(vid, box["destination"])
+                    
+                    assigned_boxes.add(bid)
+                    spawned = True
+                    break 
+                except ValueError:
+                    # This happens if the vehicle type is not allowed at this location
+                    # (e.g. trying to spawn a Truck at an Airport)
+                    continue
+            
+            if not spawned:
+                # If we couldn't spawn anything, we'll try again next tick
+                pass
